94b5dc7b9adc2b67556dc0e75fe41bab0231c554
tanyaofei <tanyaofei@outlook.com>
2023-07-05 21:11:38 +0800

    修改变量

--- src/main/java/io/github/hello09x/quiz/support/StatisticPlaceholder.java
+++ src/main/java/io/github/hello09x/quiz/support/StatisticPlaceholder.java
@@ -1,20 +1,27 @@
 package io.github.hello09x.quiz.support;
 
 import io.github.hello09x.quiz.repository.StatisticRepository;
-import io.github.hello09x.quiz.repository.model.Statistic;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
+import java.util.regex.Pattern;
 
 public class StatisticPlaceholder extends PlaceholderExpansion {
+
+    /**
+     * top.1.name
+     * top.1.corrects
+     * top.2.corrects
+     */
+    private final static Pattern PATTERN = Pattern.compile(
+            "^top\\.(?<ordinal>\\d+)\\.(?<type>name|corrects)$"
+    );
 
     private final StatisticRepository repository = StatisticRepository.instance;
 
     private final static String VERSION = "1";
-
 
     @Override
     public @NotNull String getIdentifier() {
@@ -33,25 +40,25 @@ public class StatisticPlaceholder extends PlaceholderExpansion {
 
     @Override
     public @Nullable String onPlaceholderRequest(Player player, @NotNull String params) {
-        if (!params.startsWith("corrects.top.")) {
+        var matcher = PATTERN.matcher(params);
+        if (!matcher.find()) {
             return params;
         }
 
-        var n = params.substring("corrects.top.".length()).trim();
-        if (n.isBlank()) {
-            return params;
+        var ordinal = Integer.parseInt(matcher.group("ordinal"));
+        var type = matcher.group("type");
+
+        var stat = repository.selectTop(ordinal);
+        if (stat == null) {
+            return "";
         }
 
-        int ordinal;
-        try {
-            ordinal = Integer.parseInt(n);
-        } catch (NumberFormatException e) {
-            return params;
-        }
-
-        return Optional
-                .ofNullable(repository.selectTop(ordinal))
-                .map(Statistic::playerName)
-                .orElse("");
+        return switch (type) {
+            case "name" -> stat.playerName();
+            case "corrects" -> String.valueOf(stat.corrects());
+            default -> "Invalid type: " + type;
+        };
     }
+
+
 }
