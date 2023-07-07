641d7492c8d3eb2ae8c1357c51d5246e5e731216
tanyaofei <tanyaofei@outlook.com>
2023-07-07 09:58:13 +0800

    fix connection

--- src/main/java/io/github/hello09x/quiz/Quiz.java
+++ src/main/java/io/github/hello09x/quiz/Quiz.java
@@ -69,6 +69,6 @@ public final class Quiz extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        AbstractRepository.closeConnections();
+        AbstractRepository.closeConnection(this);
     }
 }
