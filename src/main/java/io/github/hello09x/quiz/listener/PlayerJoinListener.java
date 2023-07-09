14ac5eca45ac5b0dec3065a021705648c7a6d18f
tanyaofei <tanyaofei@outlook.com>
2023-07-09 15:12:23 +0800

    添加 README

--- src/main/java/io/github/hello09x/quiz/listener/PlayerJoinListener.java
+++ src/main/java/io/github/hello09x/quiz/listener/PlayerJoinListener.java
@@ -2,10 +2,12 @@ package io.github.hello09x.quiz.listener;
 
 import io.github.hello09x.quiz.manager.QuizManager;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import static net.kyori.adventure.text.format.NamedTextColor.DARK_GREEN;
+import static net.kyori.adventure.text.format.NamedTextColor.YELLOW;
 
 public class PlayerJoinListener implements Listener {
 
@@ -19,7 +21,10 @@ public class PlayerJoinListener implements Listener {
         if (asking != null) {
             var player = event.getPlayer();
             asking.getProgress().getBossBar().addPlayer(player);
-            player.sendMessage(Component.text("[quiz] 正在提问，快回答！题目是: \n" + asking.getQuestion().title(), NamedTextColor.YELLOW));
+            player.sendMessage(Component.textOfChildren(
+                    Component.text("[quiz] 正在提问，快回答！题目是: ", YELLOW),
+                    Component.text(asking.getQuestion().title(), DARK_GREEN))
+            );
         }
     }
 
